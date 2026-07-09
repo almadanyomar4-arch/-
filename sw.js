20d5a6a55b3680108c187174fc64d64d89d32440
fawzi <mosfawzi14@gmail.com>
2026-07-10 01:53:50 +0200

    v1.4.2: fix mobile export + OneDrive error handling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sub-cache-v1.4.1";
+const CACHE_NAME = "sub-cache-v1.4.2";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
