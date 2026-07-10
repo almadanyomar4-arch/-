c556091e34c17add1fcb0ce4fc2156e4fcad0bcf
fawzi <mosfawzi14@gmail.com>
2026-07-10 02:19:24 +0200

    v1.4.3: export as modal+copy+share (works in WebView)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sub-cache-v1.4.2";
+const CACHE_NAME = "sub-cache-v1.4.3";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
